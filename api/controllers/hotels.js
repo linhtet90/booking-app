d3916a353e95bdcf5946c4ddc5bf6b6dbb1a1de5
linhtet90 <linhtet.23@ichat.sp.edu.sg>
2024-12-18 13:35:19 +0800

    refactor hotel controllers to use async/await for improved readability and error handling

--- api/controllers/hotels.js
+++ api/controllers/hotels.js
@@ -1,62 +1,52 @@
 import { createError } from '../util/error.js';
 import { createHotel, updateHotel, deleteHotel, getAllHotels, getHotelById } from '../models/hotels.js'
 
-export const createHotelController = (req, res, next) => {
-    const data = req.body;
+export const createHotelController = async (req, res, next) => {
+    try {
+        const data = req.body;
+        const hotel = await createHotel(data);
+        res.status(201).json(hotel);
+    } catch (err) {
+        next(err);
+    }
+};
 
-    createHotel(data)
-        .then((hotel) => {
-            res.status(201).json(hotel);
-        })
-        .catch((err) => {
-            next(err);
-        })
-}
+export const updateHotelController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const hotel = await updateHotel(id, data);
+        res.status(200).json(hotel);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export const updateHotelController = (req, res, next) => {
-    const { id } = req.params;
+export const deleteHotelController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await deleteHotel(id);
+        res.status(200).json("Hotel has been deleted.");
+    } catch (err) {
+        next(err);
+    }
+};
 
-    const data = req.body;
+export const getAllHotelsController = async (req, res, next) => {
+    try {
+        const hotels = await getAllHotels();
+        res.status(200).json(hotels);
+    } catch (err) {
+        next(err);
+    }
+};
 
-    updateHotel(id, data)
-        .then((hotel) => {
-            res.status(200).json(hotel);
-        })
-        .catch((err) => {
-            next(err);
-        })
-}
-
-export const deleteHotelController = (req, res, next) => {
-    const { id } = req.params;
-
-    deleteHotel(id)
-        .then(() => {
-            res.status(200).json("Hotel has been deleted.");
-        })
-        .catch((err) => {
-            next(err);
-        })
-}
-
-export const getAllHotelsController = (req, res, next) => {
-    getAllHotels()
-        .then((hotels) => {
-            res.status(200).json(hotels);
-        })
-        .catch((err) => {
-            next(err);
-        })
-}
-
-export const getHotelByIdController = (req, res, next) => {
-    const { id } = req.params;
-
-    getHotelById(id)
-        .then((hotel) => {
-            res.status(200).json(hotel);
-        })
-        .catch((err) => {
-            next(err);
-        })
-}
+export const getHotelByIdController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const hotel = await getHotelById(id);
+        res.status(200).json(hotel);
+    } catch (err) {
+        next(err);
+    }
+};
