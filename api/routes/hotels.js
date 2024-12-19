a2ff35e9ed22b48b915306ef5d4ee2f53cb4be8b
linhtet90 <linhtet.23@ichat.sp.edu.sg>
2024-12-19 21:41:00 +0800

    add admin verification middleware to hotel routes for create, update, and delete operations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { createHotelController, updateHotelController, deleteHotelController, getAllHotelsController, getHotelByIdController } from '../controllers/hotels.js'
+import { verifyAdmin, verifyToken } from '../utils/verifyToken.js';
 
 
 const router = express.Router();
 
-router.post('/', createHotelController);
+router.post('/', verifyToken, verifyAdmin, createHotelController);
 
-router.put('/:id', updateHotelController);
+router.put('/:id', verifyToken, verifyAdmin, updateHotelController);
 
-router.delete('/:id', deleteHotelController);
+router.delete('/:id', verifyToken, verifyAdmin, deleteHotelController);
 
 router.get('/', getAllHotelsController);
 
