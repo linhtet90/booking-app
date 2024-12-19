709786ebd2c733a36020d1b84570268bfaef8fd3
linhtet90 <linhtet.23@ichat.sp.edu.sg>
2024-12-19 21:38:28 +0800

    implement user authorization middleware for token verification and role checks

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import { updateUserController, deleteUserController, getAllUsersController, getUserByIdController } from '../controllers/users.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.put('/:id', updateUserController);
+// router.get('/check-authentication', verifyToken, (req, res, next) => {
+//     res.send('You are authenticated!');
+// })
 
-router.delete('/:id', deleteUserController);
+// router.get('/verify-user/:id', verifyToken, verifyUser, (req, res, next) => {
+//     res.send('You can edit your profile!');
+// })
 
-router.get('/', getAllUsersController);
+// router.get('/verify-admin', verifyToken, verifyAdmin, (req, res, next) => {
+//     res.send('You are admin');
+// })
 
-router.get('/:id', getUserByIdController);
+router.put('/:id', verifyToken, verifyUser, updateUserController);
+
+router.delete('/:id', verifyToken, verifyUser, deleteUserController);
+
+router.get('/', verifyToken, verifyAdmin, getAllUsersController);
+
+router.get('/:id', verifyToken, verifyUser, getUserByIdController);
 
 export default router;
---
+++ api/utils/verifyToken.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import 'dotenv/config'
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) {
+        return next(createError(401, "You are not authenticated!"));
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        //invalid token
+        if (err) {
+            return next(createError(403, "Invalid token!"));
+        }
+
+        req.user = decoded;
+        next();
+    })
+}
+
+export const verifyUser = (req, res, next) => {
+    if ((req.user.id == req.params.id) || req.user.isAdmin) {
+        next();
+    }
+    else {
+        return next(createError(403, "You are not authorized!"));
+    }
+}
+
+export const verifyAdmin = (req, res, next) => {
+    if (req.user.isAdmin) {
+        next();
+    }
+    else {
+        return next(createError(403, "You are not authorized!"));
+    }
+}
